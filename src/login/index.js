86027df7aa243dbc164a61f08a1dbee4905885d3
hoainambrvt2001 <hoainambrvt2001@gmail.com>
2024-12-12 06:23:30 +0700

    Add rule redirecting auth

--- src/login/index.js
+++ src/login/index.js
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import LoginForm from './components/LoginForm';
+import useAuth from '../../hooks/useAuth';
 import PageLayout from '../../components/PageLayout';
 
 const Login = () => {
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (user) {
+      router.replace('/user');
+      return null;
+    }
+  }, [user]);
+
   return (
     <PageLayout isHiddenNav={true}>
       <LoginForm />
--- src/user/index.js
+++ src/user/index.js
@@ -1,7 +1,20 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 import UserProfile from './components/UserProfile';
+import useAuth from '../../hooks/useAuth';
 import PageLayout from '../../components/PageLayout';
 
 function User() {
+  const router = useRouter();
+  const { user } = useAuth();
+
+  useEffect(() => {
+    if (!user) {
+      router.replace('/login');
+      return null;
+    }
+  }, [user]);
+
   return (
     <PageLayout>
       <UserProfile />
